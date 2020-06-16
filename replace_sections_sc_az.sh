7f3d7d2de2379704ad3873dc7e8665eeba415afc
Pablo Moreno <pablo.a.moreno@gmail.com>
2020-06-16 22:49:39 +0100

    Modify both single cell files

--- replace_sections_sc_az.sh
+++ replace_sections_sc_az.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-SOURCE_FILE=single-cell-ebi-gxa.yaml.lock
-sed 's/\(tool_panel_section_id:\).*\(hca_sc_.*\)$/\1 hca_sc_all/' $SOURCE_FILE > sc_mod_az.yaml.lock
-rm $SOURCE_FILE
+for s in single-cell-ebi-gxa single-cell-IUC; do
+  SOURCE_FILE=$s.yaml.lock
+  echo "Modifying $SOURCE_FILE..."
+  sed 's/\(tool_panel_section_id:\).*\(hca_sc_.*\)$/\1 hca_sc_all/' $SOURCE_FILE > $s.mod.yaml.lock
+  rm $SOURCE_FILE
+done
