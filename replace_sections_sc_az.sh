8dac4a678dcff8255e77153419ecca29ead4f901
Pablo Moreno <pablo.a.moreno@gmail.com>
2020-07-02 22:05:12 +0100

    IUC SC and bgruening's text manipulation to hc_sc_all and bgruenings.

--- replace_sections_sc_az.sh
+++ replace_sections_sc_az.sh
@@ -3,6 +3,10 @@
 for s in single-cell-ebi-gxa single-cell-IUC; do
   SOURCE_FILE=$s.yaml.lock
   echo "Modifying $SOURCE_FILE..."
-  sed 's/\(tool_panel_section_id:\).*\(hca_sc_.*\)$/\1 hca_sc_all/' $SOURCE_FILE > $s.mod.yaml.lock
+  sed 's/\(tool_panel_section_id:\).*\(hca_sc_.*\)$/\1 hca_sc_all/' $SOURCE_FILE | \
+      sed "s/tool_panel_section_label: 'Text Manipulation'/tool_panel_section_id: data_manipulation/" | \
+      sed "s/tool_panel_section_label: 'RNA-Seq'/tool_panel_section_id: hca_sc_all" | \
+      sed "s/tool_panel_section_label: FASTA/FASTQ manipulation/tool_panel_section_id: hca_sc_all" \
+	  > $s.mod.yaml.lock
   rm $SOURCE_FILE
 done
